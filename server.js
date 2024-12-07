a882721d1e234da70740a49004c690511144e7a2
Deepanshu325 <deepanshushrivastav999@gmail.com>
2024-12-07 14:26:20 +0530

    2nd

--- server.js
+++ server.js
@@ -90,5 +90,5 @@ Adventure Rides Team`,
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
